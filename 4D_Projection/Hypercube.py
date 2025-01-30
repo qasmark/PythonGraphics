8a765308ac2ec9322f4fb55d407dd496e46cd54f
qasmark <99826972+qasmark@users.noreply.github.com>
2025-01-30 07:12:44 +0300

    fixed animation problems

--- 4D_Projection/Hypercube.py
+++ 4D_Projection/Hypercube.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 from PyQt5 import QtWidgets, QtCore
 from OpenGL.GL import *
@@ -17,6 +16,26 @@ class HypercubeVisualizer(QtWidgets.QOpenGLWidget):
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_animation)
         self.timer.start(16)
+
+        # Инициализация вершин и ребер
+        self.vertices = self.generate_vertices()
+        self.edges = self.generate_edges()
+
+    def generate_vertices(self):
+        return np.array([
+            [-1, -1, -1, -1], [1, -1, -1, -1], [1, 1, -1, -1], [-1, 1, -1, -1],
+            [-1, -1, 1, -1], [1, -1, 1, -1], [1, 1, 1, -1], [-1, 1, 1, -1],
+            [-1, -1, -1, 1], [1, -1, -1, 1], [1, 1, -1, 1], [-1, 1, -1, 1],
+            [-1, -1, 1, 1], [1, -1, 1, 1], [1, 1, 1, 1], [-1, 1, 1, 1]
+        ], dtype=np.float32)
+
+    def generate_edges(self):
+        return [
+            (0, 1), (1, 2), (2, 3), (3, 0), (4, 5), (5, 6), (6, 7), (7, 4),
+            (0, 4), (1, 5), (2, 6), (3, 7), (8, 9), (9, 10), (10, 11), (11, 8),
+            (12, 13), (13, 14), (14, 15), (15, 12), (8, 12), (9, 13), (10, 14), (11, 15),
+            (0, 8), (1, 9), (2, 10), (3, 11), (4, 12), (5, 13), (6, 14), (7, 15)
+        ]
 
     def update_animation(self):
         self.angle += 2
@@ -38,6 +57,13 @@ class HypercubeVisualizer(QtWidgets.QOpenGLWidget):
         glEnable(GL_LIGHT1)
         glClearColor(0.9, 0.9, 0.9, 1.0)
 
+        # Настройка материалов
+        glMaterialfv(GL_FRONT, GL_AMBIENT, [0.2, 0.2, 0.2, 1.0])
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, [0.8, 0.8, 0.8, 1.0])
+        glMaterialfv(GL_FRONT, GL_SPECULAR, [1.0, 1.0, 1.0, 1.0])
+        glMaterialf(GL_FRONT, GL_SHININESS, 100.0)
+
+        # Настройка освещения
         glLightfv(GL_LIGHT1, GL_POSITION, [0, 5, 0, 1.0])
         glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, [0, -1, 0])
         glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 30.0)
@@ -65,44 +91,30 @@ class HypercubeVisualizer(QtWidgets.QOpenGLWidget):
         self.draw_ground_plane()
         self.draw_hypercube()
         self.draw_shadow()
-        
+
         glFlush()
 
     def draw_hypercube(self):
         radius = 5.0
         x = radius * np.cos(np.radians(self.position_angle))
         z = radius * np.sin(np.radians(self.position_angle))
+
         glPushMatrix()
         glTranslatef(x, 0.0, z)
         glRotatef(self.angle, 1, 1, 0)
         glScalef(self.scale_factor, self.scale_factor, self.scale_factor)
 
-        vertices = np.array([
-            [-1, -1, -1, -1], [1, -1, -1, -1], [1, 1, -1, -1], [-1, 1, -1, -1],
-            [-1, -1, 1, -1], [1, -1, 1, -1], [1, 1, 1, -1], [-1, 1, 1, -1],
-            [-1, -1, -1, 1], [1, -1, -1, 1], [1, 1, -1, 1], [-1, 1, -1, 1],
-            [-1, -1, 1, 1], [1, -1, 1, 1], [1, 1, 1, 1], [-1, 1, 1, 1]
-        ])
-        
-        rotation_matrix_4d = self.rotation_matrix_4d(self.angle * np.pi / 180, self.angle * 2 * np.pi / 180)
-        projected_vertices = np.array([self.project_vertex(v, rotation_matrix_4d) for v in vertices])
+        # 4D вращение
+        rotation_matrix = self.rotation_matrix_4d()
+        projected_vertices = [self.project_vertex(v, rotation_matrix) for v in self.vertices]
 
-        edges = [
-            (0, 1), (1, 2), (2, 3), (3, 0),
-            (4, 5), (5, 6), (6, 7), (7, 4),
-            (0, 4), (1, 5), (2, 6), (3, 7),
-            (8, 9), (9, 10), (10, 11), (11, 8),
-            (12, 13), (13, 14), (14, 15), (15, 12),
-            (8, 12), (9, 13), (10, 14), (11, 15),
-            (0, 8), (1, 9), (2, 10), (3, 11),
-            (4, 12), (5, 13), (6, 14), (7, 15)
-        ]
-
-        glBegin(GL_LINES)
+        # Отрисовка ребер
         glColor3f(0.2, 0.2, 0.8)
-        for edge in edges:
-            for vertex in edge:
-                glVertex3fv(projected_vertices[vertex][:3])
+        glLineWidth(2.0)
+        glBegin(GL_LINES)
+        for edge in self.edges:
+            for idx in edge:
+                glVertex3fv(projected_vertices[idx][:3].astype(np.float32).tobytes())
         glEnd()
 
         glPopMatrix()
@@ -110,78 +122,64 @@ class HypercubeVisualizer(QtWidgets.QOpenGLWidget):
     def draw_shadow(self):
         glPushMatrix()
         glColor3f(0.1, 0.1, 0.1)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
         radius = 5.0
         x = radius * np.cos(np.radians(self.position_angle))
         z = radius * np.sin(np.radians(self.position_angle))
-        glTranslatef(x, 0.0, z)  
-        glRotatef(self.angle, 1, 1, 0)  
 
-        vertices = np.array([
-            [-1, -1, -1, -1], [1, -1, -1, -1], [1, 1, -1, -1], [-1, 1, -1, -1],
-            [-1, -1, 1, -1], [1, -1, 1, -1], [1, 1, 1, -1], [-1, 1, 1, -1],
-            [-1, -1, -1, 1], [1, -1, -1, 1], [1, 1, -1, 1], [-1, 1, -1, 1],
-            [-1, -1, 1, 1], [1, -1, 1, 1], [1, 1, 1, 1], [-1, 1, 1, 1]
-        ])
-        
-        rotation_matrix_4d = self.rotation_matrix_4d(self.angle * np.pi / 180, self.angle * 2 * np.pi / 180)
-        projected_vertices = np.array([self.project_vertex(v, rotation_matrix_4d) for v in vertices])
+        glTranslatef(x, 0.0, z)
+        glRotatef(self.angle, 1, 1, 0)
 
-        edges = [
-            (0, 1), (1, 2), (2, 3), (3, 0),
-            (4, 5), (5, 6), (6, 7), (7, 4),
-            (0, 4), (1, 5), (2, 6), (3, 7),
-            (8, 9), (9, 10), (10, 11), (11, 8),
-            (12, 13), (13, 14), (14, 15), (15, 12),
-            (8, 12), (9, 13), (10, 14), (11, 15),
-            (0, 8), (1, 9), (2, 10), (3, 11),
-            (4, 12), (5, 13), (6, 14), (7, 15)
-        ]
+        rotation_matrix = self.rotation_matrix_4d()
+        projected_vertices = [self.project_vertex(v, rotation_matrix) for v in self.vertices]
 
         glBegin(GL_LINES)
-        for edge in edges:
-            for vertex in edge:
-                shadow_vertex = projected_vertices[vertex].copy()
-                shadow_vertex[1] = -3.0  
-                glVertex3fv(shadow_vertex[:3]) 
+        for edge in self.edges:
+            for idx in edge:
+                vert = projected_vertices[idx].copy()
+                vert[1] = -3.0
+                glVertex3fv(vert[:3].astype(np.float32).tobytes())
         glEnd()
 
+        glDisable(GL_BLEND)
         glPopMatrix()
 
     def draw_ground_plane(self):
         glPushMatrix()
-        glTranslatef(0.0, -10.0, 0.0)
-        glBegin(GL_QUADS)
+        glTranslatef(0.0, -5.0, 0.0)
         glColor3f(0.7, 0.7, 0.7)
-        glVertex3f(-10, 0, -10)
-        glVertex3f(10, 0, -10)
-        glVertex3f(10, 0, 10)
-        glVertex3f(-10, 0, 10)
+        glBegin(GL_QUADS)
+        size = 20
+        glVertex3f(-size, 0, -size)
+        glVertex3f(size, 0, -size)
+        glVertex3f(size, 0, size)
+        glVertex3f(-size, 0, size)
         glEnd()
         glPopMatrix()
 
-    def rotation_matrix_4d(self, theta, phi):
-        cos_t, sin_t = np.cos(theta), np.sin(theta)
-        cos_p, sin_p = np.cos(phi), np.sin(phi)
-        
-        rotation_matrix = np.array([
-            [cos_t * cos_p, -sin_t, -sin_p, 0],
-            [sin_t, cos_t * cos_p, 0, -sin_p],
-            [0, 0, cos_t, sin_t],
-            [sin_p, 0, -sin_t, cos_p]
-        ])
-        return rotation_matrix
+    def rotation_matrix_4d(self):
+        theta = np.radians(self.angle)
+        phi = np.radians(self.angle * 2)
+
+        return np.array([
+            [np.cos(theta) * np.cos(phi), -np.sin(theta), -np.sin(phi), 0],
+            [np.sin(theta), np.cos(theta) * np.cos(phi), 0, -np.sin(phi)],
+            [0, 0, np.cos(theta), np.sin(theta)],
+            [np.sin(phi), 0, -np.sin(theta), np.cos(phi)]
+        ], dtype=np.float32)
 
     def project_vertex(self, vertex, rotation_matrix):
         rotated = np.dot(rotation_matrix, vertex)
-        perspective = 3 / (4 - rotated[3])
+        perspective = 3.0 / (4.0 - rotated[3])
         return rotated[:3] * perspective
 
 
 class HypercubeApp(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("4D Hypercube Visualization with Spotlight and Shadow")
+        self.setWindowTitle("4D Hypercube Visualization (Fixed)")
         self.setGeometry(100, 100, 800, 600)
         self.visualizer = HypercubeVisualizer()
         self.setCentralWidget(self.visualizer)
